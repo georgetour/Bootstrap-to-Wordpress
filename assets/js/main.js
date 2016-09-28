33dae1f9792454cf14d54d5e7dcdbeb30cb67c97
George Tourtsinakis <tourtsinakis@outlook.com>
2016-09-28 14:54:04 +0300

    Modal layout ready
    - Added layout modal for quote
    - Tested and looks nice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,27 @@
-/**
- * Created by George on 06-Sep-16.
- */
+$(document).ready(function () {
+
+    //Cache the window object
+    var $window = $(window);
+
+    //Parallax background effect
+    $('section[data-type="background"]').each(function () {
+        var $backgroundObject = $(this);//assigning the object
+
+        $(window).scroll(function () {
+
+        //scroll the background at var speed
+        //the yPos is a negative value because we are scrolling it up
+        var yPos = -($window.scrollTop()/$backgroundObject.data('speed'));
+
+        //Put together out background position
+        var coords = '50%' + yPos + 'px';
+
+       //Move the background
+        $backgroundObject.css({backgroundPosition:coords});
+
+        });//End Window scroll
+
+    });
+
+
+});
